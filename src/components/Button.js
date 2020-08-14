f3391ce00f637c56b558c84cfc88cce60efb238c
Roberto <robertochirila@yahoo.com>
2020-08-14 12:42:08 +0100

    fixed conditional

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,35 +8,64 @@ import hit from "../actions/hit";
 // this component will be connected to the store and will dispatch actions to the store
 
 class Button extends Component {
-  shuffleDeck = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disableButton: false,
+    };
+  }
+  shuffleDeck = (event) => {
     const { deck, step } = this.props;
-    this.props.shuffleDeck(deck);
-    this.props.incrementStep();
+    if (event.target.value === "shuffle") {
+      this.props.shuffleDeck(deck);
+      this.props.incrementStep();
+    }
   };
-  handleHit = () => {
+  handleHit = (event) => {
     console.log("hereeeeeeeeee");
-    this.props.incrementStep();
+    if (event.target.value === "hit") {
+      this.props.incrementStep();
+    }
   };
-  handleStay = () => {
+  handleStay = (event) => {
     console.log("hereeeeeeeeee");
-    this.props.incrementStep();
+    if (event.target.value === "stay") {
+      this.setState({ disableButton: true });
+    }
+
+    //this.props.incrementStep();
   };
   render() {
-    const { type } = this.props;
+    const { type, playerScore, dealerScore } = this.props;
+    const { disableButton } = this.state;
     return (
       <React.Fragment>
         <input
           type="button"
           value={type}
+          disabled={disableButton}
           onClick={
             type === "shuffle"
               ? this.shuffleDeck
-              : this.handleHit || type === "stay"
+              : type === "hit"
+              ? this.handleHit
+              : type === "stay"
               ? this.handleStay
               : null
           }
           className="button"
         />
+        {disableButton === true ? (
+          <div>
+            <h4>Your score is: {playerScore}</h4>
+            <h4>Dealer score is: {dealerScore}</h4>
+            {playerScore > dealerScore ? (
+              <p>Congratulations you won !</p>
+            ) : (
+              <p>You lost, long live the dealer !</p>
+            )}
+          </div>
+        ) : null}
       </React.Fragment>
     );
   }
@@ -44,6 +73,8 @@ class Button extends Component {
 
 const mapStateToProps = (state) => ({
   deck: state.deck.deck,
+  playerScore: state.player.playerScore,
+  dealerScore: state.dealer.dealerScore,
   step: state.step.step,
 });
 
